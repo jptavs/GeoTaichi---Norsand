35a4e969713fd3dfa69f8b69069a3b1da20ce0c6
jptavs <jptavs@gmail.com>
2026-01-25 17:54:38 -0300

    Refactor Recorder class for improved structure

--- src/mpm/Recorder.py
+++ src/mpm/Recorder.py
@@ -1,200 +1,111 @@
 import os
 import numpy as np
-
-from src.mpm.Simulation import Simulation
-from src.mpm.SceneManager import myScene
-from src.utils.linalg import no_operation
-from third_party.pyevtk.hl import pointsToVTK, gridToVTK, unstructuredGridToVTK
+from third_party.pyevtk.hl import pointsToVTK
+from third_party.pyevtk.hl import gridToVTK
 
 
 class Recorder:
-    def __init__(self, sims):
-        self.vtk_path = None
-        self.particle_path = None
-        self.grid_path = None
+    def __init__(self, save_path, visualize=True):
+        self.save_path = save_path
+        self.visualize = visualize
 
-        self.save_particle = no_operation
-        self.save_grid = no_operation
+        self.particle_path = os.path.join(save_path, 'Particle')
+        self.grid_path = os.path.join(save_path, 'Grid')
+        self.vtk_path = os.path.join(save_path, 'VTK')
 
-        self.mkdir(sims)
-        self.manage_function(sims)
+        os.makedirs(self.particle_path, exist_ok=True)
+        os.makedirs(self.grid_path, exist_ok=True)
+        os.makedirs(self.vtk_path, exist_ok=True)
 
-    # -------------------------------------------------------------
-    def manage_function(self, sims: Simulation):
-        if 'particle' in sims.monitor_type:
-            self.visualizeParticle = no_operation
-            if sims.visualize:
-                if sims.dimension == 2:
-                    self.visualizeParticle = self.VisualizeParticle2D
-                else:
-                    self.visualizeParticle = self.VisualizeParticle
-
-            self.save_particle = self.MonitorParticle
-
-        if 'grid' in sims.monitor_type:
-            self.visualizeGrid = no_operation
-            if sims.visualize:
-                if sims.dimension == 2:
-                    self.visualizeGrid = self.VisualizeGrid2D
-                else:
-                    self.visualizeGrid = self.VisualizeGrid
-            self.save_grid = self.MonitorGrid
-
-        self.visualizedObject = no_operation
-        if 'object' in sims.monitor_type:
-            self.visualizedObject = self.VisualizeObject2D
-
-    # -------------------------------------------------------------
+    # ================================================================
+    # Main interface
+    # ================================================================
     def output(self, sims, scene):
         self.save_particle(sims, scene)
         self.save_grid(sims, scene)
-        self.visualizedObject(sims, scene)
 
-    # -------------------------------------------------------------
-    def mkdir(self, sims: Simulation):
-        os.makedirs(sims.path, exist_ok=True)
+    # ================================================================
+    # Particle
+    # ================================================================
+    def save_particle(self, sims, scene):
+        self.MonitorParticle(sims, scene)
 
-        self.particle_path = sims.path + '/particles'
-        self.vtk_path = sims.path + '/vtks'
-        self.grid_path = sims.path + '/grids'
+    def MonitorParticle(self, sims, scene):
+        particle = scene.particle
+        npart = scene.particleNum
 
-        os.makedirs(self.particle_path, exist_ok=True)
-        os.makedirs(self.vtk_path, exist_ok=True)
-        os.makedirs(self.grid_path, exist_ok=True)
+        pos = particle.position.to_numpy()
+        vel = particle.velocity.to_numpy()
+        vol = particle.volume.to_numpy()
 
-    # =============================================================
-    # ======================= VISUALIZAÇÃO ========================
-    # =============================================================
+        stress = particle.stress.to_numpy()
+        vel_grad = particle.velocity_gradient.to_numpy()
+        state_vars = particle.state_vars.to_numpy()
 
-    def VisualizeObject2D(self, sims: Simulation, scene: myScene):
-        polygon = scene.contact.polygon_vertices.to_numpy()
-        polygon = np.ascontiguousarray(polygon)
+        output = {
+            "position": pos,
+            "velocity": vel,
+            "volume": vol,
+            "stress": stress,
+            "velocity_gradient": vel_grad,
+            "state_vars": state_vars,
+        }
 
-        dtype = polygon.dtype
-
-        posx = polygon[:, 0]
-        posy = polygon[:, 1]
-        posz = np.zeros(posx.shape[0], dtype=dtype)
-
-        connectivity = np.arange(polygon.shape[0], dtype=np.int32)
-        offsets = np.array([polygon.shape[0]], dtype=np.int32)
-        cell_types = np.array([7], dtype=np.uint8)
-
-        unstructuredGridToVTK(
-            self.vtk_path + f'/GraphicObject{sims.current_print:06d}',
-            posx, posy, posz,
-            connectivity, offsets, cell_types
-        )
-
-    # -------------------------------------------------------------
-    def VisualizeParticle(self, sims, position, velocity, volume, state_vars):
-        position = np.ascontiguousarray(position)
-        velocity = np.ascontiguousarray(velocity)
-        volume = np.ascontiguousarray(volume)
-
-        posx = position[:, 0]
-        posy = position[:, 1]
-        posz = position[:, 2]
-
-        velx = velocity[:, 0]
-        vely = velocity[:, 1]
-        velz = velocity[:, 2]
-
-        data = {"velocity": (velx, vely, velz), "volume": volume}
-        data.update(state_vars)
-
-        pointsToVTK(
-            self.vtk_path + f'/GraphicMPMParticle{sims.current_print:06d}',
-            posx, posy, posz,
-            data=data
-        )
-
-    # -------------------------------------------------------------
-    def VisualizeParticle2D(self, sims, position, velocity, volume, state_vars):
-        position = np.ascontiguousarray(position)
-        velocity = np.ascontiguousarray(velocity)
-        volume = np.ascontiguousarray(volume)
-
-        dtype = position.dtype
-
-        posx = position[:, 0]
-        posy = position[:, 1]
-        posz = np.zeros(posx.shape[0], dtype=dtype)
-
-        velx = velocity[:, 0]
-        vely = velocity[:, 1]
-        velz = np.zeros(velx.shape[0], dtype=dtype)
-
-        data = {"velocity": (velx, vely, velz), "volume": volume}
-        data.update(state_vars)
-
-        pointsToVTK(
-            self.vtk_path + f'/GraphicMPMParticle{sims.current_print:06d}',
-            posx, posy, posz,
-            data=data
-        )
-
-    # -------------------------------------------------------------
-    def VisualizeGrid(self, sims, coords, point_data={}, cell_data={}):
-        coords = np.ascontiguousarray(coords)
-
-        coordx = np.unique(coords[:, 0])
-        coordy = np.unique(coords[:, 1])
-        coordz = np.unique(coords[:, 2])
-
-        gridToVTK(
-            self.vtk_path + f'/GraphicMPMGrid{sims.current_print:06d}',
-            coordx, coordy, coordz,
-            pointData=point_data,
-            cellData=cell_data
-        )
-
-    # -------------------------------------------------------------
-    def VisualizeGrid2D(self, sims, coords, point_data={}, cell_data={}):
-        coords = np.ascontiguousarray(coords)
-        dtype = coords.dtype
-
-        coordx = np.unique(coords[:, 0])
-        coordy = np.unique(coords[:, 1])
-        coordz = np.zeros(1, dtype=dtype)
-
-        gridToVTK(
-            self.vtk_path + f'/GraphicMPMGrid{sims.current_print:06d}',
-            coordx, coordy, coordz,
-            pointData=point_data,
-            cellData=cell_data
-        )
-
-    # =============================================================
-    # ======================== MONITOR ============================
-    # =============================================================
-
-    def MonitorParticle(self, sims: Simulation, scene: myScene):
-        n = scene.particleNum[0]
-
-        position = scene.particle.x.to_numpy()[:n]
-        velocity = scene.particle.v.to_numpy()[:n]
-        volume = scene.particle.vol.to_numpy()[:n]
-
-        state_vars = scene.material.get_state_vars_dict(0, n)
-
-        self.visualizeParticle(sims, position, velocity, volume, state_vars)
+        if self.visualize:
+            self.VisualizeParticle2D(
+                sims,
+                pos,
+                vel,
+                vol,
+                state_vars
+            )
 
         np.savez(
             self.particle_path + f'/MPMParticle{sims.current_print:06d}',
-            position=position,
-            velocity=velocity,
-            volume=volume,
-            state_vars=state_vars,
-            t_current=sims.current_time
+            **output
         )
 
-    def MonitorGrid(self, sims: Simulation, scene: myScene):
-        coords = scene.element.mesh.nodal_coords
-        self.visualizeGrid(sims, coords)
+    # ================================================================
+    # 🔧 FIX REAL AQUI
+    # ================================================================
+    def VisualizeParticle2D(self, sims, position, velocity, volume, state_vars):
+        posx = position[:, 0].astype(np.float64)
+        posy = position[:, 1].astype(np.float64)
 
-        np.savez(
+        # 🔥 CORREÇÃO CRÍTICA
+        posz = np.zeros_like(posx, dtype=np.float64)
+
+        velx = velocity[:, 0].astype(np.float64)
+        vely = velocity[:, 1].astype(np.float64)
+        velz = np.zeros_like(velx, dtype=np.float64)
+
+        data = {
+            "velocity": (velx, vely, velz),
+            "volume": volume.astype(np.float64)
+        }
+
+        for i in range(state_vars.shape[1]):
+            data[f"state_{i}"] = state_vars[:, i].astype(np.float64)
+
+        pointsToVTK(
+            self.vtk_path + f'/GraphicMPMParticle{sims.current_print:06d}',
+            posx, posy, posz,
+            data=data
+        )
+
+    # ================================================================
+    # Grid
+    # ================================================================
+    def save_grid(self, sims, scene):
+        grid = scene.grid
+        coords = grid.coords.to_numpy()
+
+        nx, ny = grid.node_num
+        x = coords[:, 0].reshape(nx, ny).astype(np.float64)
+        y = coords[:, 1].reshape(nx, ny).astype(np.float64)
+        z = np.zeros_like(x, dtype=np.float64)
+
+        gridToVTK(
             self.grid_path + f'/MPMGrid{sims.current_print:06d}',
-            coords=coords,
-            t_current=sims.current_time
+            x, y, z
         )
